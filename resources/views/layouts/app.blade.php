e450dcb72899505a25fa740ec27ac34d521b7907
WadhahEssam <wadah-wleed@hotmail.com>
2018-06-06 04:27:29 +0300

    new

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="ar">
+<head>
+    <title> Min Challenge - FTC</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/style.css" >
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
+    <script src="scripts/script.js"></script>
+    {{--todo : the shortcut icon must be changed--}}
+    <link rel="shortcut icon"  href="images/chlogo.png">
+
+</head>
+<body>
+
+</body>
+</html>`
