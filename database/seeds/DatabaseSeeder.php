14aec79603504bf0ca6f7a2a06673329843974e4
WadhahEssam <wadah-wleed@hotmail.com>
2018-07-02 04:10:20 +0300

    pc

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,30 +14,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         for the questions database
-        for ( $i = 0 ; $i < $this->numberOfQuestions ; $i++)
-            DB::table('questions' )->insert([
-                'question' =>  "This is a test question number " . ($i+1) ,
-                'option1' => $this->createOption() ,
-                'option2' => $this->createOption(),
-                'option3' => $this->createOption(),
-                'option4' => $this->createOption(),
-                'imagePath' => '' ,
-                'answer' => rand(1,4) ,
-                'dif' => rand(1,3) ,
-            ]);
+//         for the questions database
+//        for ( $i = 0 ; $i < $this->numberOfQuestions ; $i++)
+//            DB::table('questions' )->insert([
+//                'question' =>  "This is a test question number " . ($i+1) ,
+//                'option1' => $this->createOption() ,
+//                'option2' => $this->createOption(),
+//                'option3' => $this->createOption(),
+//                'option4' => $this->createOption(),
+//                'imagePath' => '' ,
+//                'answer' => rand(1,4) ,
+//                'dif' => rand(1,3) ,
+//            ]);
 
-         for the results database
-        for ( $i = 0 ; $i < $this->numberOfResults ; $i++)
-            DB::table('results' )->insert([
-                'first_student_name' => str_random(rand(3,8)) ,
-                'first_student_id' => rand(430000000 , 438000000) ,
-                'first_student_points' => rand(20,200),
-                'second_student_name' => str_random(rand(3,8)) ,
-                'second_student_id' => rand(430000000 , 438000000) ,
-                'second_student_points' => rand(20,200),
-                'winner' => rand(1,2),
-            ]);
+//         for the results database
+//        for ( $i = 0 ; $i < $this->numberOfResults ; $i++)
+//            DB::table('results' )->insert([
+//                'first_student_name' => str_random(rand(3,8)) ,
+//                'first_student_id' => rand(430000000 , 438000000) ,
+//                'first_student_points' => rand(20,200),
+//                'second_student_name' => str_random(rand(3,8)) ,
+//                'second_student_id' => rand(430000000 , 438000000) ,
+//                'second_student_points' => rand(20,200),
+//                'winner' => rand(1,2),
+//            ]);
 
         DB::table('running_games')->insert([
             'user_1_ready' => '0' ,
